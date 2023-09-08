145d759c39790a03868cb3b765c2198fa35c760c
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-09-08 21:41:18 +0300

    Added changes to 15

--- 15.c
+++ 15.c
@@ -8,15 +8,15 @@ int main()
     unsigned short (int) 0  65535
     */
     printf("SHRT_MIN = %d\n", SHRT_MIN);
-    printf("SHRT_MAX = %u\n", SHRT_MAX);
+    printf("SHRT_MAX = %d\n", SHRT_MAX);
     printf("USHRT_MAX = %u\n\n", USHRT_MAX);
 
     /* long (long int) 2^32 = 4 294 967 296
     -2147483648     2147483647
-    unsignet long int 0     4 294 967 295
+    unsigned long int 0     4 294 967 295
     */
-    printf("LONG_MIN = %d\n", LONG_MIN);
-    printf("LONG_MAX = %u\n", LONG_MAX);
-    printf("ULONG_MAX = %u\n\n", ULONG_MAX);
+    printf("LONG_MIN = %ld\n", LONG_MIN);
+    printf("LONG_MAX = %ld\n", LONG_MAX);
+    printf("ULONG_MAX = %lu\n\n", ULONG_MAX);
     return 0;
 }

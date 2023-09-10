42ff70dfaaded4b26d410775faa700ce386a55e0
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-09-10 18:27:56 +0300

    Added 21

---
+++ 21.c
@@ -0,0 +1,11 @@
+// Hungarian notation
+#include <stdio.h>
+
+int main()
+{
+    unsigned uRashod = 777;
+    int iSaldo = 888;
+    unsigned char uc_Voltage = 220;
+    printf("uc_Voltage = %d volt", uc_Voltage);
+    return 0;
+}

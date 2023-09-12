5773eb7c20a11bf5e003244dcf7218b0d51c8b02
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-09-12 19:44:14 +0300

    Added changes 24

--- 24.c
+++ 24.c
@@ -4,10 +4,10 @@ int main()
 {
     unsigned num = 1000;
     unsigned char test = 200;
-    unsigned long long int test_1 = 10000;
+    long long int test_1 = 10000;
 
-    printf("Var num is %u, size is %u bytes, address is %p\n", num, sizeof(num), &num);
-    printf("Var test is %u, size is %u bytes, address is %p\n", test, sizeof(test), &test);
-    printf("Var test_1 is %d, size is %lld bytes, address is %p", test_1, sizeof(test_1), &test_1);
+    printf("Var num is %d, size is %u bytes, address is %p\n", num, sizeof(num), &num);
+    printf("Var test is %d, size is %u bytes, address is %p\n", test, sizeof(test), &test);
+    printf("Var test_1 is %lld, size is %u bytes, address is %p", test_1, sizeof(test_1), &test_1);
     return 0;
 }

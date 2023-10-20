a3036a9e2a525829765b2af6755e514c18b6e1e9
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-10-20 18:26:54 +0300

    Added changes to 66.1

--- 66.1.c
+++ 66.1.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
 #include <windows.h>
 
-void bendingCenterCalc(int heightUbeam, int widthUbeam, double shelfThickness, double wallThickness, char ch1, char ch2, char ch3)
-{
-    double part1 = 0.0;
-    double part2 = 0.0;
-    double bendingCenter = 0.0;
-    part1 = 3 * (double)(widthUbeam - 0.5 * wallThickness) * (widthUbeam - 0.5 * wallThickness);
-    part2 = (heightUbeam - shelfThickness) + 6 * (widthUbeam - 0.5 * wallThickness);
-    bendingCenter = -(part1 / part2) + 0.5 * wallThickness;
-
-    printf("U-beam %c%c%c, the center of the bend relative to the outer face of the shelf %.1f mm\n", ch1, ch2, ch3, bendingCenter);
-}
+void bendingCenterCalc(int heightUbeam, int widthUbeam, double shelfThickness,
+                       double wallThickness, char ch1, char ch2, char ch3);
 
 int main()
 {
@@ -29,9 +20,23 @@ int main()
     puts("the bending center of the U-beams with parallel faces of the shelves ***");
     puts("Enter the name of the U-beam, its height, width, shelf and wall thickness in mm separated by a space: ");
     scanf(" %c %c %c %d %d %lf %lf", &ch1, &ch2, &ch3, &heightUbeam, &widthUbeam, &shelfThickness, &wallThickness);
+    fflush(stdin);
 
     bendingCenterCalc(heightUbeam, widthUbeam, shelfThickness, wallThickness, ch1, ch2, ch3);
     system("pause>nul");
 
     return 0;
+}
+
+void bendingCenterCalc(int heightUbeam, int widthUbeam, double shelfThickness,
+                       double wallThickness, char ch1, char ch2, char ch3)
+{
+    double part1 = 0.0;
+    double part2 = 0.0;
+    double bendingCenter = 0.0;
+    part1 = 3 * (double)(widthUbeam - 0.5 * wallThickness) * (widthUbeam - 0.5 * wallThickness);
+    part2 = (heightUbeam - shelfThickness) + 6 * (widthUbeam - 0.5 * wallThickness);
+    bendingCenter = -(part1 / part2) + 0.5 * wallThickness;
+
+    printf("U-beam %c%c%c, the center of the bend relative to the outer face of the shelf %.1f mm\n", ch1, ch2, ch3, bendingCenter);
 }

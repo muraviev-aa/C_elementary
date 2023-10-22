d65f1a4d3a3052a8a6a5fea6ab053e0a91aee5f0
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-10-22 14:52:04 +0300

    Added 25

---
+++ 25/25/25.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int var = 7;
+	unsigned long address;
+	// The first way to find the address
+	printf("Address of var is %X\n", (int)&var);
+
+	// The second way to find the address
+	__asm
+	{
+		LEA EBX, var
+		MOV address, EBX
+	}
+	printf("Address of var is %X\n", address);
+
+	return 0;
+}

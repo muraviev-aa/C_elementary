3249c5b31c2d1be1edda938da1efc1f7c9ce5bc3
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-11-11 09:47:27 +0300

    Update 98.c

--- 98.c
+++ 98.c
@@ -1,4 +1,4 @@
-// -Wl,--stack,20971520
+// -Wl,--stack,20971520 // 20 MB stack memory allocation
 
 #include <stdio.h>
 
@@ -6,7 +6,7 @@ int main()
 {
     char massiv[2084544] = {0};
     char number;
-    printf("C is tne best prog lang!\n");
+    printf("C is the best prog lang!\n");
     printf("Address of massiv[0] is %u\n", &massiv[0]);
     printf("Address of number is %u\n", &number);
     return 0;
